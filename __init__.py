f13dbe8fb5a8c34aa18a7761ad27f38a7e0bae30
cattykitty4 <aspectmastezzima@gmail.com>
2025-05-28 19:16:24 +1100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,3 +3,6 @@ from .rectangle_core import Rectangle
 from .triangle_core import Triangle
 
 __all__ = ('Circle', 'Rectangle', 'Triangle')
+# from geometric_figures import *
+# при таком импорте сработает переменная __all__
+# которая импортирует только переданные в нее классы
