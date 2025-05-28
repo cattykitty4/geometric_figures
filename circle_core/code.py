3fbeeb9c7d272d7ed029dd16678171cb38dfdccd
cattykitty4 <aspectmastezzima@gmail.com>
2025-05-28 18:55:55 +1100

    Update code.py

--- circle_core/code.py
+++ circle_core/code.py
@@ -13,7 +13,7 @@ class Circle:
         Примеры:
         >>> x = Circle(3)       # создание экземпляра класса Circle с передачей аргумента 3.
         >>> y = Circle(33, 40)  # создание экземпляра класса Circle с передачей аргумента 33 для радиуса и 40 для угла.
-        >>> j = Circle('33')
+        >>> j = Circle('33')    # неправильное создание экземпляра класса Circle
         Traceback (most recent call last):
         ...
         TypeError: Вводимое число должно быть флоатером или целым числом
@@ -57,6 +57,7 @@ class Circle:
         >>> y.circle_area().__round__(2)
         12.57
         """
+        
         # Площадь (S) высчитывается из геометрической формулы. Формула: S = 2 * pi * R ** 2
         s = math.pi * math.pow(self.radius, 2)
         
@@ -72,9 +73,11 @@ class Circle:
         >>> y.get_chord_length().__round__(2)
         1.37
         """
+        
         # Хорда высчитывается исходя из геометрической формулы по нахождению длины хорды,
         # когда известен радиус и величина угла в радиусах. Формула: AB = 2 * R * sin(alpha / 2)
         chord_length = 2 * self.radius * math.sin(self.angle / 2)
+        
         return chord_length
 
 
